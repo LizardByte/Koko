a8171df316a47207dd4cbef26157b20306b774e7
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2025-03-14 21:08:33 -0400

    tests: improve db fixture

--- tests/test_web/routes/auth.rs
+++ tests/test_web/routes/auth.rs
@@ -1,59 +1,39 @@
+// lib imports
 use rocket::http::Status;
 use rstest::rstest;
 use serde_json::json;
 use serial_test::serial;
 
+// test imports
 use crate::fixtures;
 use crate::test_web::test_request;
 
 #[rstest]
 #[serial(db)]
 #[tokio::test]
-#[case::login_success("testuser", "testuser", "password123", "password123", Status::Ok)]
-#[case::login_invalid(
-    "testuser",
-    "nonexistent",
-    "password123",
-    "wrong",
-    Status::Unauthorized
-)]
+#[case::login_success("admin", "password123", Status::Ok)]
+#[case::login_invalid("nonexistent", "wrong", Status::Unauthorized)]
 async fn test_login(
     #[future]
     #[from(fixtures::db_fixture)]
+    #[with(true)]
     db_future: fixtures::TestDb,
-    #[case] username_0: &str,
-    #[case] username_1: &str,
-    #[case] password_0: &str,
-    #[case] password_1: &str,
+    #[case] username: &str,
+    #[case] password: &str,
     #[case] expected_status: Status,
 ) {
     let db = db_future.await;
-    let client = &db.client;
-
-    // Create test user
-    test_request(
-        "post",
-        "/create_user",
-        Some(json!({
-            "username": username_0,
-            "password": password_0,
-            "admin": false
-        })),
-        Status::Ok,
-        Some(client),
-    )
-    .await;
 
     // Test login
     let response = test_request(
         "post",
         "/login",
         Some(json!({
-            "username": username_1,
-            "password": password_1
+            "username": username,
+            "password": password,
         })),
         expected_status,
-        Some(client),
+        Some(&db.client),
     )
     .await;
 
